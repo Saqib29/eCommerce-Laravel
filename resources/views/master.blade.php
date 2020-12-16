b27dad6713ea6abe93afb4c00c46e27b91145991
Saqib <saqib29111@gmail.com>
2020-12-16 21:37:12 +0600

    Add Header, Footer and Login page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -15,13 +15,14 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    {{ View::make('header') }}
+    @yield('content')
+    {{ View::make('footer') }}
 </body>
-<script>
-    $(document).ready(function(){
-        $("button").click(function(){
-            alert('all set');
-        });
-    });
-</script>
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px;
+    }
+</style>
 </html>
