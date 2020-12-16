a3e65c9cc5631630b99e4015061ad8857060e675
Saqib <saqib29111@gmail.com>
2020-12-17 03:20:48 +0600

    Product detail page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -16,11 +16,13 @@
         <div class="carousel-inner">
           @foreach ($products as $item)
             <div class="item {{ $item['id']==1 ? 'active' : '' }}">
-                <img class="slider-img" src="{{ $item->gallery }}" alt="{{ $item->name }}">
-                <div class="carousel-caption slider-text">
-                <h3>{{ $item->name }}</h3>
-                <p>{{ $item->description }}</p>
-                </div>
+                <a href="detail/{{ $item->id }}">
+                    <img class="slider-img" src="{{ $item->gallery }}" alt="{{ $item->name }}">
+                    <div class="carousel-caption slider-text">
+                    <h3>{{ $item->name }}</h3>
+                    <p>{{ $item->description }}</p>
+                    </div>
+                </a>
             </div>
           @endforeach
       
@@ -37,15 +39,17 @@
         </a>
       </div>
       <div class="trending-wrapper">
-          <h3>Trending Products</h3>
-          @foreach($products as $item)
-          <div class="trending-item">
-            <img class="trending-img" src="{{ $item->gallery }}">
-            <div class="">
-            <h3>{{ $item->name }}</h3>
-            </div>
-        </div>
-      @endforeach
+            <h3>Trending Products</h3>
+            @foreach($products as $item)
+                <div class="trending-item">
+                    <a href="detail/{{ $item->id }}">
+                        <img class="trending-img" src="{{ $item->gallery }}">
+                        <div class="">
+                        <h3>{{ $item->name }}</h3>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
   
     </div>
       </div>
